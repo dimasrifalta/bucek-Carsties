a4a3878ad324e942f7dafa5a5075d24173e7d735
dimasrifalta <dimasrifalta@gmail.com>
2024-05-12 17:10:32 +0700

    End of section 5

--- src/Contracts/obj/Debug/net7.0/Contracts.AssemblyInfo.cs
+++ src/Contracts/obj/Debug/net7.0/Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95dac49be7ff86e08a293c5f8ba50ff9e7435c60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d5baeec7ac92974ea0ae1a828b79a859a440a83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
